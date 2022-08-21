ae223fb4995c992553a7bfebf5b9494363cddd69
Jason Varbedian <jpvarbed@gmail.com>
2022-08-20 18:31:58 -0700

    Shuffle

--- leetcode/variousproblems.cpp
+++ leetcode/variousproblems.cpp
@@ -32,6 +32,66 @@ public:
     }
 };
 
+/*
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+
+Input
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+Output
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+                       // Any permutation of [1,2,3] must be equally likely to be returned.
+                       // Example: return [3, 1, 2]
+solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+Note that the input is a ref, not const so you must change the passed in deck
+*/
+
+#include <algorithm>
+#include <random>       // std::default_random_engine
+#include <chrono>       // std::chrono::system_clock
+
+class ShuffleSolution {
+public:
+    ShuffleSolution(vector<int>& nums) : _originalNums(nums) {
+         // std::vector<int> ivec(nums.size());
+        //std::iota(ivec.begin(), ivec.end(), 0);
+        _positions = _originalNums;
+    }
+    
+    vector<int> reset() {
+        _originalNums = _positions;
+        return _originalNums;
+    }
+    
+    vector<int> shuffle() {
+        auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::shuffle(_originalNums.begin(), _originalNums.end(), std::default_random_engine(seed));
+        return _originalNums;
+    }
+private:
+    vector<int> &_originalNums;
+    vector<int> _positions;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
+
 int main() {
 	int test[] = { 1, 7, 4, 6, 3, 10, 2 };
     Solution sol;
